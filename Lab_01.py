2bbaa9574083778b14605bf7048a899f84f19ed5
Tekurukichi <146697844+Tekurukichi@users.noreply.github.com>
2024-06-12 18:08:13 +0300

    Update Lab_01.py

--- Lab_01.py
+++ Lab_01.py
@@ -1,18 +1,50 @@
-import ipaddress
+#Task1
+a = int(input("Введіть ціле число для змінної a: "))
+b = float(input("Введіть дробове число для змінної b: "))
+c = int(input("Введіть ціле число для змінної c: "))
+d = float(input("Введіть дробове число для змінної d: "))
+print("Змінна a:", a)
+print("Змінна b:", b)
+print("Змінна c:", c)
+print("Змінна d:", d)
 
-def generate_network_commands(start_ip, end_ip, subnet_mask):
-    network_address = ipaddress.ip_network((start_ip, end_ip), subnet_mask)
-    for network in network_address.subnets():
-        network_command = f"network {network.network_address} {network.netmask}"
-        yield network_command
+#Task2
+addition = a + d
+subtraction = a - b
+multiplication = a * d
+division = a / b
+exponentiation = c ** b
+floor_division = a // b
+remainder = a % d
+results = [addition, subtraction, multiplication, division, exponentiation, floor_division, remainder]
+print("Результати дій:")
+print(results)
 
-def main():
-    start_ip = input("Введіть початкову IP-адресу: ")
-    end_ip = input("Введіть кінцеву IP-адресу: ")
-    subnet_mask = input("Введіть маску підмережі: ")
+#Task3
+number_of_elements = len(results)
+print("Кількість елементів у списку:", number_of_elements)
 
-    for network_command in generate_network_commands(start_ip, end_ip, subnet_mask):
-        print(network_command)
+# Виведення парних елементів
+print("Парні елементи списку:")
+for element in results:
+    if element % 2 == 0:
+        print(element)
 
-if __name__ == "__main__":
-    main()
+#Task4
+results[1], results[4] = results[4], results[1]
+
+# Виведення отриманого списку
+print("Отриманий список:")
+print(results)
+
+#Task5
+name = input("Введіть ваше прізвище та ім'я: ")
+
+print("Виконавець лабораторної роботи:", name)
+print("Висновки по лабораторній роботі:")
+print("У цій роботі я навчився:")
+print("- Опрацьовувати введення даних від користувача.")
+print("- Виконувати арифметичні дії над числами.")
+print("- Створення та маніпулювання списками.")
+print("- Доступ до елементів списку та їх зміна.")
+print("- Виведення даних на екран.")
